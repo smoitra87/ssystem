2a0fd6aea8f557d1d01a077b4fa951b5a4b6c83c
Subhodeep Moitra <subhodeep.moitra@gmail.com>
2012-01-08 13:56:10 +0530

    Added profile class skeleton to base.py

--- ssystem/base.py
+++ ssystem/base.py
@@ -6,22 +6,31 @@ Base class that contains a number of important classes
 
 
 class ssystem : 
-	""" Defines the ssystem """
+	""" The ssystem class """
 	def __init__(self) : 
 		self.isdefined = False;
 	def set_params(self,**args) :
 		pass
 
 class experiment : 
+	""" The experiment class """
 	def __init__(self) :
 		self.isdefined = False;
 	def set_params(self,**args) : 
 		pass
 
 class equation :
+	""" The equation class """
 	def __init__(self) : 
 		self.isdefined = False;
 	def set_params(self,**args) : 
+		pass
+
+class profile : 
+	""" The biochemical profile class """
+	def __init__(self) : 
+		self.isdefined = False;
+	def set_params(self.=,**args) :
 		pass
 
 
