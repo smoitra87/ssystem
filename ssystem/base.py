06addcf0d8d45f6688693d0b23958efd0ecaf850
Subhodeep Moitra <subhodeep.moitra@gmail.com>
2012-01-08 12:50:57 +0530

    Created skeletons for ssystem, experiment, equation in base.py

---
+++ ssystem/base.py
@@ -0,0 +1,27 @@
+#! /usr/bin/env python
+
+"""
+Base class that contains a number of important classes
+"""
+
+
+class ssystem : 
+	""" Defines the ssystem """
+	def __init__(self) : 
+		self.isdefined = False;
+	def set_params(self,**args) :
+		pass
+
+class experiment : 
+	def __init__(self) :
+		self.isdefined = False;
+	def set_params(self,**args) : 
+		pass
+
+class equation :
+	def __init__(self) : 
+		self.isdefined = False;
+	def set_params(self,**args) : 
+		pass
+
+
