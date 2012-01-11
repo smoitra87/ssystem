74019e43c16d53f56a86545d55e42911197514cf
Subhodeep Moitra <subhodeep.moitra@gmail.com>
2012-01-11 23:01:55 +0530

    Expanded SSystem, Experiment and Profile classes in base.py

--- ssystem/base.py
+++ ssystem/base.py
@@ -7,36 +7,49 @@ author:Subhodeep Moitra(smoitra@cs.cmu.edu)
 BSD License
 """
 
+import numpy as np
 
-class ssystem : 
+class SSystem(object) : 
 	""" The ssystem class """
-	def __init__(self) : 
-		self.isdefined = False;
+	def __init__(self,data) : 
+		self.name = data['problemname']
+		self.systype = data['type']
+		self.date = data['date']
+		self.url = data['url']
+		self.variables = data['variables']
+		self.errorfn = data['errorfunc']
+		self.experiments = [Experiment(exp) for exp in data['experiments']]
+		self.constraint = None;
 	def set_params(self,**args) :
 		pass
 
-class experiment : 
+class Experiment(object) : 
 	""" The experiment class """
-	def __init__(self) :
-		self.isdefined = False;
+	def __init__(self,exp) :
+		self.id = exp['id']
+		self.name = exp['name']
+		self.datatype = exp['datatype']
+		self.profile = Profile(exp['samples'])
 	def set_params(self,**args) : 
 		pass
 
-class equation :
+class Equation(object) :
 	""" The equation class """
 	def __init__(self) : 
 		self.isdefined = False;
 	def set_params(self,**args) : 
 		pass
 
-class profile : 
+class Profile(object) : 
 	""" The biochemical profile class """
-	def __init__(self) : 
-		self.isdefined = False;
+	def __init__(self,samples) : 
+		self.time = np.array([sample['time'] for sample in samples])
+		self.var = np.array([sample['var'] for sample in samples])
+		self.sdev =np.array( [sample['sdev'] for sample in samples])
 	def set_params(self,**args) :
 		pass
 
-class constraint : 
+class Constraint(object) : 
 	"""The experiment constraint class """
 	def __init__(self) : 
 		self.isdefined = False;
