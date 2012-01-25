7508afd7b53abcfe670469477c3865303a3038c4
Subhodeep Moitra <smoitra@cs.cmu.edu>
2012-01-25 17:14:15 -0500

    Added other tests for Chou2006 experiment 1

--- tests/other_models_tests.py
+++ tests/other_models_tests.py
@@ -2,6 +2,8 @@ from nose.tools import *
 from ssystem.other_models import Chou2006
 from ssystem.base import Constraint
 from ssystem.utility import same_dict
+import numpy as np
+import sys
 
 class TestChou2006(object) :
 	def setUp(self) : 
@@ -54,8 +56,65 @@ class TestChou2006(object) :
 
 	def test_experiment1(self) :
 		""" Test Chou2006 first experiment"""
-		pass
+		# Check nPoints
+		ss = self.ss
+		e = ss.experiments[0]
+		p = e.profile
+		eq_(p.n_sample,50)
+		eq_(p.n_var,4)
+		eq_(p.var.shape,(p.n_sample,p.n_var))
+		eq_(len(p.time),p.n_sample)
+		# check profile time accurate to 2 decimal places
+		assert_f = lambda t : assert_almost_equal(t[0],t[1],places=2)
+		map(assert_f,zip(p.time,np.arange(0,5,0.1)))
+		eq_(p.time.shape,(p.n_sample,))
+		# Check initial point
+		assert_f = lambda t : assert_almost_equal(t[0],t[1],places=2)
+		map(assert_f,zip(ss._y0,[1.4,2.7,1.2,0.4]))
+		eq_(ss._t0,0)
 
+		# check some random points accurate to 3 decimal places
+		assert_f = lambda t : assert_almost_equal(t[0],t[1],places=3)
+		test_vars = [
+			[ 1.4       ,  2.7       ,  1.2       ,  0.4       ],#0th
+			[ 0.32173054,  2.43086341,  2.6428591 ,  0.13536515], #10th
+			[ 0.33980311,  1.8215696 ,  2.41620586,  0.12619968], # 20th
+			[ 0.41154004,  1.95905513,  2.17694858,  0.14416145],# 30th
+			[ 0.40633804,  2.03137886,  2.21211375,  0.14475924], # 40th
+			[ 0.39767285,  2.01128965,  2.23608434,  0.14247661]] #49th	
+		for i,j in enumerate((0,10,20,30,40,49)) : 
+			map(assert_f,zip(test_vars[i],p.var[j]))
+
+	def test_exp1_fitted1(self) : 
+		"""Compares fitted vs true slopes with 1 decimal accuracy"""
+		# check simulated slopes and fitted slopes are accurate to 
+		# 2 decimal places
+		ss = self.ss
+		p = ss.experiments[0].profile
+		assert_f = lambda t : assert_almost_equal(t[0],t[1],places=1)
+		assert_f2 = lambda t : map(assert_f,zip(t[0],t[1]))
+		map(assert_f2,zip(ss._slope,p.slopes))
+
+		# check simulates slopes don't
+	
+	def test_exp1_fitted2(self) : 
+		""" Compares fitted bs true slopes with 2 decimal accuracy , 
+			Supposed to fail"""	
+		ss = self.ss
+		p = ss.experiments[0].profile
+		assert_f = lambda t : assert_almost_equal(t[0],t[1],places=2)
+		assert_f2 = lambda t : map(assert_f,zip(t[0],t[1]))
+		
+		try : 
+			map(assert_f2,zip(ss._slope,p.slopes))
+			assert False
+		except AssertionError :
+			sys.stdout.write("Supposed to fail at 2 decimal places\n")	
+			pass
+	
+
+
+		
 	def test_constraint(self) : 
 		""" Test Chou2006 constraints """
 		ss = self.ss
