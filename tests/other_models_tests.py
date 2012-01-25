00053ce40c0dc69e5382a5667200afd56cdd3f8a
Subhodeep Moitra <smoitra@cs.cmu.edu>
2012-01-24 20:35:42 -0500

    Added some tests for testing Chou2006

---
+++ tests/other_models_tests.py
@@ -0,0 +1,132 @@
+from nose.tools import *
+from ssystem.other_models import Chou2006
+from ssystem.base import Constraint
+from ssystem.utility import same_dict
+
+class TestChou2006(object) :
+	def setUp(self) : 
+		"""Setup fixture for the test"""
+		self.ss = Chou2006() # create ss instance
+				
+	
+	def tearDown(self) :
+		pass
+	
+	def test_keys(self) :
+		""" Test Chou2006 keys"""
+		ss = self.ss
+		assert ss.name == 'chou2006'
+		assert ss.systype == 'SSystem'
+		eq_(ss.date,"01/01/12")
+		eq_(ss.url,"http://www.cs.cmu.edu/~subhodee")
+	
+	def test_variables(self) :
+		""" Test Chou2006 variables"""
+		# number of vars is 5
+		ss = self.ss
+		var_list = [
+		{'id':1,'name':'x1','property':'dependent'},
+		{'id':2,'name':'x2','property':'dependent'},
+		{'id':3,'name':'x3','property':'dependent'},
+		{'id':4,'name':'x4','property':'dependent'}
+		]
+		assert len(var_list) == len(ss.variables)
+		for ii in range(len(var_list)) :
+			var_test = var_list[ii]
+			var_ss = ss.variables[ii]
+			assert len(var_ss) == len(var_test)
+			eq_(var_ss['id'],var_test['id'])
+			eq_(var_ss['name'],var_test['name'])
+			eq_(var_ss['property'],var_test['property'])
+
+	def test_errorfunc(self) : 
+		""" Test Chou2006 errorfunc"""
+		ss = self.ss
+		erf = ss.errorfn
+		eq_(erf['equation'],'-L+lambda*K')
+		assert_almost_equal(erf['lambda'],1.0)
+		eq_(erf['type'],'minusLogLikelihoodPlusLambdaK')
+
+	def test_experiment(self) :
+		""" Test some general Chou2006 exp properties"""
+		ss = self.ss
+		assert_greater(len(ss.experiments),0)
+
+	def test_experiment1(self) :
+		""" Test Chou2006 first experiment"""
+		pass
+
+	def test_constraint(self) : 
+		""" Test Chou2006 constraints """
+		ss = self.ss
+		cons = ss.constraint.modelspace
+		test_cons = gen_constraint()
+		test_model = test_cons.modelspace
+		assert same_dict(test_model.alpha,cons.alpha)
+		assert same_dict(test_model.beta,cons.beta)
+		assert same_dict(test_model.g,cons.g)
+		assert same_dict(test_model.h,cons.h)
+
+		cons = ss.constraint.initsol
+		test_initsol = test_cons.initsol
+		assert same_dict(test_initsol.alpha,cons.alpha)
+		assert same_dict(test_initsol.beta,cons.beta)
+		assert same_dict(test_initsol.g,cons.g)
+		assert same_dict(test_initsol.h,cons.h)
+		
+
+def gen_constraint() :
+	modelspace = gen_modelspace()
+	initbound = gen_initbound()
+	initsol = gen_initsol()
+	constraint = Constraint(modelspace,initbound,initsol)
+	return constraint
+
+
+def gen_modelspace():
+	modelspace = {
+		'alpha': {
+			'defaultLowerBound' : 0.0,
+			'defaultUpperBound' : 15.0
+		},	
+		'beta': {
+			'defaultLowerBound' : 0.0,
+			'defaultUpperBound' : 15.0
+		},
+		'g': {
+			'defaultLowerBound' : -3.0,
+			'defaultUpperBound' : 3.0
+		},
+		'h': {
+			'defaultLowerBound' : -3.0,
+			'defaultUpperBound' : 3.0
+		}
+	}
+	return modelspace
+
+def gen_initbound() :
+	initbound = {
+		'alpha' : {},
+		'beta' : {},
+		'g' : {},
+		'h' : {}
+	}
+	return initbound
+
+def gen_initsol() :
+	initsol = {
+		'alpha' : {
+			'defaultInitialValue' : 1.0
+		},
+		'beta' : {
+			'defaultInitialValue' : 1.0
+		},
+		'g': {
+			'defaultInitiValue' : 0.0
+		},
+		'h' : {
+			'defaultInitialValue' : 0.0
+		}
+	}
+	return initsol
+
