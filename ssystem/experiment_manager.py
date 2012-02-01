42a57c2ca81a47688dd4b13f6f1fc51f40f7d35d
Subhodeep Moitra <subhodeep.moitra@gmail.com>
2012-01-31 20:48:56 -0500

    Added schedules and scenarios to experiment_manager

--- ssystem/experiment_manager.py
+++ ssystem/experiment_manager.py
@@ -15,25 +15,40 @@ class ExperimentManager(object) :
 	""" Class for managing all experiments """
 	def __init__(self) : 
 		self.pman = ParserManager()
+		self.schedule = self.scenario_schedule()
 	def run_scenario1(self) : 
 		""" Runs experiments for Chou2006"""
-		pass
+		for ss in self.pman.get_gen_chou2006() :
+			print "Running %s" %(ss.name)
+
 	def run_scenario2(self) : 
 		""" Runs experiments for ss_5genes1"""
-		self._ss = self.pman.get_5genes1()
+		for ss in self.pman.get_gen_5genes1() :
+			print  "Running %s" % (ss.name)
 		
-	def run_all(self) : 
+	def run_scenario3(self) : 
 		""" Runs all experiments in all s-systems"""
 		pass
 	
+	def scenario_schedule(self) : 
+		"""Gives a schedule of scenarios to run """
+		sched = [
+			self.run_scenario1,
+			self.run_scenario2
+		]
+		
+		return sched
+
 	def set_params(self,**args) : 
 		""" Sets params of the Experiment Manager Class"""
 		pass
 
 	def start(self) : 
 		""" Starts execution of Exoeriment Engine"""
-		pass
+
+		for run_scenario in self.schedule : 
+			run_scenario()
 
 if __name__ == '__main__'  :
 	e = ExperimentManager()
-	e.run_scenario2()
+	e.start()
